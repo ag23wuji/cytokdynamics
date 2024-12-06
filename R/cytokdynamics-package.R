b89f8b07a8428a14b495ece160033b826bc7fa95
Godber Godbersen <godbergodbersen@MacBook-Pro-von-Godber.local>
2024-12-06 17:44:12 +0100

    added package dependencies

---
+++ R/cytokdynamics-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom glue glue_collapse
+#' @importFrom nlme VarCorr
+#' @importFrom performance check_collinearity
+## usethis namespace: end
+NULL
